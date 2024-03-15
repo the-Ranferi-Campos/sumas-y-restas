de06f2a52a1439e0ade7df57f70246de1f7b121d
the-Ranferi-Campos <152442798+the-Ranferi-Campos@users.noreply.github.com>
2024-03-15 08:30:11 -0600

    Add files via upload

---
+++ funciones html/expresiones.js
@@ -0,0 +1,10 @@
+function saludar() {
+const nombre = "UNID";
+console.log("hola", nombre);
+}
+
+
+function saludarlos (){
+    const nombreDos = prompt("ingresar un nombre");
+    console.log("hola", nombreDos)
+}
---
+++ funciones html/funciones.js
@@ -0,0 +1,13 @@
+function AreaCuadrado(lado) {
+    return lado*lado
+}
+
+//una funcion se invoca llamando el nombre y pasando el argumento
+AreaCuadrado(5);
+
+//pero yo necesito ver el valor para ello usuare: console
+console.log(AreaCuadrado(5));
+
+//o tambien paver ver una alreta en mi navegador usando
+//usando alert()
+alert(AreaCuadrado(5));
